5f0db07c2fca43b9aa7a2dcf8e59162319445591
upunks0 <105484571+upunks0@users.noreply.github.com>
2022-05-15 13:06:38 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,66 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNOFFICIAL PUNKSt</title>
+    <style>
+        body {
+            background: rgb(225, 226, 216);
+        }
+        main {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding-bottom: 50px;
+        }
+        h1 {
+            color: rgb(0, 0, 0);
+            font-size: 30px;
+            margin-bottom: 0px;
+        }
+        p { 
+            color: rgb(255, 0, 0);
+            font-size: 30px;
+            margin-bottom: 0px;
+        }
+        h1 + p {
+            font-size: 25px;
+        }
+        #thirdweb-embed {
+            width: 100%;
+            height: auto;
+            text-align: center;
+        }
+    </style>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+</head>
+<body>
+    <main id="main">
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
 
-      <Footer />
-    </div>
-  )
-}
+        <h1>UNOFFICIAL PUNKS</h1>
+        
+        <p>MINT! MINT! MINT! MINT!</p>
+        
+
+        <div id="thirdweb-embed">
+            
+            
+            <iframe
+src="https://gateway.ipfscdn.io/ipfs/Qma17oTrNVQqPTg8v3E5UvHYFbXjbgeqHCocDZtPyUopLX/drop.html?contract=0x545df7517802D042e0308d7190973DaCa684A596&chainId=137"
+width="600px"
+height="600px"
+style="max-width:100%;"
+frameborder="0"
+></iframe>
+
+        </div>
+    </main>
+</body>
+</html>
